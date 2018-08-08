501cd37cbdabfc8c6b98ddf456985a0e6409db23
Eva Prakash <evaprakash2@gmail.com>
2018-08-07 18:02:52 -0700

    Adding minor changes

--- examples/madelon/dataset/download_stuff.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir MADELON
-cd MADELON
-wget https://archive.ics.uci.edu/ml/machine-learning-databases/madelon/MADELON/madelon_test.data
-wget https://archive.ics.uci.edu/ml/machine-learning-databases/madelon/MADELON/madelon.param
-wget https://archive.ics.uci.edu/ml/machine-learning-databases/madelon/MADELON/madelon_train.data
-wget https://archive.ics.uci.edu/ml/machine-learning-databases/madelon/MADELON/madelon_train.labels
-wget https://archive.ics.uci.edu/ml/machine-learning-databases/madelon/MADELON/madelon_valid.data

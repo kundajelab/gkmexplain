a7221a1018612966b85f5ae1dae14f85b8a3f015
AvantiShri <avanti.shrikumar@gmail.com>
2018-10-21 15:51:17 -0700

    explore dsqtl dataset

---
+++ dsQTL/gm12878_sequence_sets/train_gkm.sh
@@ -0,0 +1,1 @@
+~/lsgkm/bin/gkmtrain -t 3 -l 10 -k 6 -d 3 -c 10 -g 2 -T 16 -x 5 gm12878_shared.fa nullseqs_gm12878_shared.1.1.fa gkmsvm_t3_l10_k6_d3_c10_g2_t16_x5_
---
+++ dsQTL/runme.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget http://www.beerlab.org/deltasvm/downloads/gm12878_sequence_sets.tar.gz
+tar -xzf gm12878_sequence_sets.tar.gz

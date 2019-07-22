9d78493c824b7e0ce9acb0f25c83bfd7dfa8738a
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2019-07-22 04:33:44 +0200

    cleanup and updated readme

--- non_gkmexplain_ig/grab_data.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-FILE="sequences.simdata.gz"
-if [ ! -f "$FILE" ]
-then
-    wget https://github.com/AvantiShri/model_storage/raw/db919b12f750e5844402153233249bb3d24e9e9a/deeplift/genomics/sequences.simdata.gz
-else
-    echo "File sequences.simdata.gz exists already"
-fi
--- non_gkmexplain_ig/ssvmimp/backend/__init__.py
+++
@@ -1,2 +0,0 @@
-from __future__ import absolute_import
-from .theano_backend import *
